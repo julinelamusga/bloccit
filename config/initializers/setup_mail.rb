569d328439a9a0c433d9286d7d43065d93c1caa8
Juline Lamusga <juline.lamusga@gmail.com>
2013-11-03 22:41:45 +0000

    Carrierwave, Fog, & S3.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,8 @@ if Rails.env.development?
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'app18951393@heroku.com',
-    :password       => 'qhmufia3',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131029222412) do
+ActiveRecord::Schema.define(:version => 20131103210645) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20131029222412) do
     t.datetime "updated_at",                             :null => false
     t.string   "name"
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
